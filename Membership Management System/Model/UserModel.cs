7d4c575104318bae44b523ce9f31168f1e60183c
s2wuberrycake <s2wuberrycake@gmail.com>
2025-03-27 22:15:18 +0800

    test

--- Membership Management System/Model/UserModel.cs
+++ Membership Management System/Model/UserModel.cs
@@ -8,11 +8,9 @@ namespace Membership_Management_System.Model
 {
     class UserModel
     {
-        public Guid employee_id { get; set; }  // UUID (MySQL CHAR(36))
+        public int account_id { get; set; }  // Matches MySQL INT AUTO_INCREMENT
         public string username { get; set; }
-        public string password_hash { get; set; }
-        public DateTime birthday { get; set; } // Use DateTime for date fields
-        public string role { get; set; }  // 'admin' or 'staff'
-        public DateTime created_at { get; set; } // Timestamp field
+        public string password_hash { get; set; }  // Store the hashed password
+        public int role_id { get; set; }  // Foreign key from roles table
     }
 }
